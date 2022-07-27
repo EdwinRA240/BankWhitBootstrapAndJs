f0909a18ffe84d2205cf5a79f02451c7a44a6277
Edwin RA <edwinra240@gmail.com>
2022-07-27 14:21:02 -0500

    v3.0: fixed login access or validation

--- main.js
+++ main.js
@@ -1,32 +1,61 @@
 let users = [
-    {
-        id: 1,
-        name: 'John',
-        email: 'john@example.com',
-        password: '123456',
-        saldo: 220
-    }, 
-    {
-        id: 2,
-        name: 'Jane',
-        email: 'jane@example.com',
-        password: '123456',
-        saldo: 180
-    },
-    {
-        id: 3,
-        name: 'Doe',
-        email: 'doe@example.com',
-        password: '123456',
-        saldo: 640
-    }
-]
+  {
+    id: 1,
+    name: "John",
+    email: "john@example.com",
+    password: "123456",
+    saldo: 220,
+  },
+  {
+    id: 2,
+    name: "Jane",
+    email: "jane@example.com",
+    password: "123456",
+    saldo: 180,
+  },
+  {
+    id: 3,
+    name: "Doe",
+    email: "doe@example.com",
+    password: "123456",
+    saldo: 640,
+  },
+];
 
-let jsonUsers = JSON.stringify(users)
-localStorage.setItem('allUsers', jsonUsers);
-console.log(jsonUsers); //JSON
+//add at localStorage
+let jsonUsers = JSON.stringify(users);
+localStorage.setItem("allUsers", jsonUsers);
+// console.log(jsonUsers); /* JSON */
 
-let allUsers = localStorage.getItem('allUsers');
+//add at arrUsers
+let allUsers = localStorage.getItem("allUsers");
 let arrUsers = JSON.parse(allUsers);
-console.log(arrUsers);
+// console.log(arrUsers); /* Array */
 
+const btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("click", () => {
+  event.preventDefault();
+
+  const inputEmail = document.getElementById("inputEmail");
+  const inputPass = document.getElementById("inputPassword");
+  let b = 0;
+
+  const validation = arrUsers.some(
+    (user) =>
+      inputEmail.value === user.email && inputPass.value === user.password
+  );
+
+  if (validation) {
+    inputEmail.value = "";
+    inputPass.value = "";
+    alert("Inicio de seccion correcto");
+    let url = `http://127.0.0.1:5500/Proyectos/Bank/IngeBank.html`;
+    window.open(url, "_self");
+
+  } else {
+    inputEmail.value = "";
+    inputPass.value = "";
+    alert("Email or Password incorrect");
+  }
+});
