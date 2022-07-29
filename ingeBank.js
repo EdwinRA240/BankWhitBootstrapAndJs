92ac9b856f18de9b1db2a6b559a963ae436957e9
Edwin RA <edwinra240@gmail.com>
2022-07-28 21:36:45 -0500

    v4.0: finished functionality and fixed style

--- ingeBank.js
+++ ingeBank.js
@@ -1,10 +1,29 @@
-const btnLogOut = document.getElementById("btnLogOut");
+let users = [
+  {
+    id: 0,
+    name: "John",
+    email: "john@example.com",
+    password: "123456",
+    saldo: 320,
+  },
+  {
+    id: 1,
+    name: "Jane",
+    email: "jane@example.com",
+    password: "123456",
+    saldo: 180,
+  },
+  {
+    id: 2,
+    name: "Doe",
+    email: "doe@example.com",
+    password: "123456",
+    saldo: 640,
+  },
+];
 
-const balanceMax = 990;
-const balanceMin = 10;
-
-btnLogOut.addEventListener("click", () => {
-  location.href = ("./index.html");
-  // let url = `http://127.0.0.1:5500/Proyectos/Bank/index.html`;
-  // window.open(url, "_self");
-});
+//Update at localStorage
+if (localStorage === undefined) {
+  localStorage.setItem("allUsers", JSON.stringify(users));
+}; 
+// console.log(jsonUsers); /* JSON */
--- main.js
+++ main.js
@@ -1,63 +1,152 @@
-let users = [
-  {
-    id: 1,
-    name: "John",
-    email: "john@example.com",
-    password: "123456",
-    saldo: 220,
-  },
-  {
-    id: 2,
-    name: "Jane",
-    email: "jane@example.com",
-    password: "123456",
-    saldo: 180,
-  },
-  {
-    id: 3,
-    name: "Doe",
-    email: "doe@example.com",
-    password: "123456",
-    saldo: 640,
-  },
-];
+// let users = [
+//     {
+//       id: 0,
+//       name: "John",
+//       email: "john@example.com",
+//       password: "123456",
+//       saldo: 320,
+//     },
+//     {
+//       id: 1,
+//       name: "Jane",
+//       email: "jane@example.com",
+//       password: "123456",
+//       saldo: 180,
+//     },
+//     {
+//       id: 2,
+//       name: "Doe",
+//       email: "doe@example.com",
+//       password: "123456",
+//       saldo: 640,
+//     },
+//   ];
 
-//add at localStorage
-let jsonUsers = JSON.stringify(users);
-localStorage.setItem("allUsers", jsonUsers);
+//   //Update at localStorage
+//   if (false) {
+//     localStorage.setItem("allUsers", JSON.stringify(users));
+//   };
 // console.log(jsonUsers); /* JSON */
 
 //add at arrUsers
-let allUsers = localStorage.getItem("allUsers");
-let arrUsers = JSON.parse(allUsers);
+let arrUsers = JSON.parse(localStorage.getItem("allUsers"));
 // console.log(arrUsers); /* Array */
 
+//Variables
 const btnLogin = document.getElementById("btnLogin");
+const btnLogOut = document.getElementById("btnLogOut");
 
-btnLogin.addEventListener("click", () => {
-  event.preventDefault();
+const inputEmail = document.getElementById("inputEmail");
+const inputPass = document.getElementById("inputPassword");
 
-  const inputEmail = document.getElementById("inputEmail");
-  const inputPass = document.getElementById("inputPassword");
-  let b = 0;
+const balanceMax = 990;
+const balanceMin = 10;
 
-  const validation = arrUsers.some(
-    (user) =>
-      inputEmail.value === user.email && inputPass.value === user.password
-  );
+const bienvenida = document.getElementById("bienvenida");
 
-  if (validation) {
-    inputEmail.value = "";
-    inputPass.value = "";
-    alert("Inicio de seccion correcto");
-    // let url = `http://127.0.0.1:5500/Proyectos/Bank/ingeBank.html`;
-    // window.open(url, "_self");
-    location.href = ("./ingeBank.html");
-} else {
-    inputEmail.value = "";
-    inputPass.value = "";
-    alert("Email or Password incorrect");
-  }
-});
+// Validacion usuario
+if (btnLogin) {
+  btnLogin.addEventListener("click", () => {
+    event.preventDefault();
 
+    let user = arrUsers.find(
+      (user) =>
+        inputEmail.value === user.email && inputPass.value === user.password
+    );
 
+    if (user != undefined) {
+      localStorage.setItem("idUser", user.id);
+      alert("Inicio de Sesión Correcto"); //add Bootstrap alert
+      location.href = "./ingeBank.html";
+    } else {
+      inputEmail.value = "";
+      inputPass.value = "";
+      alert("Error en ingreso de contraseña o email");
+    }
+  });
+}
+
+// Log out
+if (btnLogOut) {
+  btnLogOut.addEventListener("click", () => {
+    location.href = "./index.html";
+  });
+}
+
+//Asignacion de index
+let idUser = localStorage.getItem("idUser");
+
+//Bienvenida
+if (true) {
+  bienvenida.innerHTML = `
+    Bienvenid@: ${arrUsers[idUser].name} `;
+}
+
+//Saldo
+const btnSaldo = document.getElementById("btnSaldo");
+const saldo = document.getElementById("saldoUser");
+if (btnSaldo) {
+  btnSaldo.onclick = () => {
+    saldo.innerHTML = `
+        Su saldo actual es: $${arrUsers[idUser].saldo} `;
+  };
+}
+
+//Ingresar
+const btnIngresar = document.getElementById("btnIngresar");
+const inputIngresar = document.getElementById("inputIngresar");
+const btnIngresarSaldo = document.getElementById("ingresar");
+
+if (btnIngresar) {
+  btnIngresar.addEventListener("click", () => {
+    if (btnIngresarSaldo) {
+      btnIngresarSaldo.addEventListener("click", () => {
+        if (
+          parseInt(inputIngresar.value) + arrUsers[idUser].saldo <=
+          balanceMax
+        ) {
+          arrUsers[idUser].saldo =
+            parseInt(inputIngresar.value) + arrUsers[idUser].saldo;
+          alert(`Su nuevo saldo es: ${arrUsers[idUser].saldo}`);
+          inputIngresar.value = "";
+          udateLocalStorage();
+        } else if (inputIngresar.value !== "") {
+          alert("No se pude realizar la transacion, porque el maximo es $990");
+          inputIngresar.value = "";
+        }
+      });
+    }
+  });
+}
+
+//Retirar
+const btnRetirar = document.getElementById("btnRetirar");
+const inputRetirar = document.getElementById("inputRetirar");
+const btnRetirarSaldo = document.getElementById("retirar");
+
+if (btnRetirar) {
+  btnRetirar.addEventListener("click", () => {
+    if (btnRetirarSaldo) {
+      btnRetirarSaldo.addEventListener("click", () => {
+        if (
+          arrUsers[idUser].saldo - parseInt(inputRetirar.value) >=
+          balanceMin
+        ) {
+          arrUsers[idUser].saldo =
+            arrUsers[idUser].saldo - parseInt(inputRetirar.value);
+          alert(`Su nuevo saldo es: ${arrUsers[idUser].saldo}`);
+          inputRetirar.value = "";
+          udateLocalStorage();
+        } else if (inputRetirar.value !== "") {
+          alert("No se pude realizar la transacion, porque el minimo es $10");
+          inputRetirar.value = "";
+        }
+      });
+    }
+  });
+}
+
+//Update Local Storage
+function udateLocalStorage() {
+  localStorage.setItem("allUsers", JSON.stringify(arrUsers));
+}
